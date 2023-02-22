bd4c7fa4d0db4862edbd28926661432e7dd945cb
Uthmanbello <uthmanbello88@gmail.com>
2023-02-22 06:49:51 +0100

    Add more functionality and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
-import styles from "styles/Header.module.css";
-
 const Header = () => (
   <header
-  style={{
-    padding: '20px 0',
-    lineHeight: '1.5em',
-    color: '#aeadad',
-    textAlign: 'center',
-  }}
+    style={{
+      padding: '20px 0',
+      lineHeight: '1.5em',
+      color: '#aeadad',
+      textAlign: 'center',
+    }}
   >
     <h1>todos</h1>
     <p>Items will persist in the browser local storage</p>
